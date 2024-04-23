a68f576f340da0f15f6df6429adcb00029a37a49
m-ji97 <tv5035@naver.com>
2024-04-23 16:38:01 +0900

    240423

--- bin/ex12.dart
+++ bin/ex12.dart
@@ -6,8 +6,8 @@ void main(){
   print(cals2(5,5,1));
 
   //익명함수: 화살표, 블록바디
-  var func = (int a, int b, int m) => a+b-m;
-  print(func(3,2,1));
+  var func =  (int a, int b, int m) => a+b-m;
+  print(func(3, 2, 1));
 
   /*익명함수
   ()=>1줄코드
@@ -15,12 +15,12 @@ void main(){
   */
 
   //화살표 함수
-  var func2 = (int a, int b) => a+b; //1줄, 반드시리턴있음: 화살표함수 0
+  var func2 = (int a, int b) => a+b; //1줄, 반드시리턴있음: 화살표함수 o
   print(func2(3,5));
   
   //불록바디
   //func3에는 함수의 주소가 담긴다
-  var func3 = (){
+  var func3 = (){ //여러줄,리턴있음ok : 화살표함수 x
     print("--------------------");
     print("a");
     print("b");
